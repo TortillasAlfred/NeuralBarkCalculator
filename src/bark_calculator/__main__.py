5fa92aa66eb61ac36036eac06876cef58a2d48fa
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-02-11 23:46:49 -0500

    Added good examples arg to parser

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -2,4 +2,11 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.parse_args()
+
+    parser.add_argument("--good_examples", help="Run with pre-filtered good examples",
+                        action="store_true")
+
+    args = parser.parse_args()
+
+    print("Good_examples mode {}".format("on" if args.good_examples else "off"))
+
