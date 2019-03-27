1d2f4e3d2092ba9eff3980e20324692ff77657b2
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-03-27 11:17:30 -0400

    256 images

--- src/bark_calculator_1/treatment_method/methods.py
+++ src/bark_calculator_1/treatment_method/methods.py
@@ -204,7 +204,7 @@ class BlackMask(TreatmentMethod):
 class BlackTrimmer(TreatmentMethod):
 
     def make_trimmer(self, image):
-        image = rescale(image, 1/4)
+        image = rescale(image, 1/16)
         summed_image = np.sum(image, axis=-1)
         summed_image = summed_image > 1e-3
 
@@ -338,12 +338,11 @@ class V2(TreatmentMethod):
         ws_image[~(ws_image == 1)] = 0
 
         ws_copy = np.repeat(ws_image, 16, axis=0).repeat(16, axis=1)
-        ws_1 = np.repeat(ws_image, 4, axis=0).repeat(4, axis=1)
-        treated_images.append(ws_1[first_idx:-last_idx])
+        treated_images.append(ws_image[first_idx:-last_idx])
         big_image_2 = np.copy(big_image)
         big_image[ws_copy == 1] = [0, 0, 0]
         big_image_2[~(ws_copy == 1)] = [0, 0, 0]
-        treated_images.append(rescale(image, 1/4)[first_idx:-last_idx])
+        treated_images.append(rescale(image, 1/16)[first_idx:-last_idx])
         treated_images.append(big_image)
         treated_images.append(big_image_2)
 
