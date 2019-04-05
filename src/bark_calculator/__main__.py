285b8f2a311239496d10f07402bfd0fb3bd0d3a0
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-04-04 23:07:06 -0400

    Maybe more solid early stopping params

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -170,11 +170,11 @@ def new_main():
                          metrics=['mse'],
                          loss_function=MixedLoss())
 
-        lr_schedulers = [ExponentialLR(gamma=0.90)]
-        callbacks = [EarlyStopping(patience=10)]
+        lr_schedulers = [ExponentialLR(gamma=0.95)]
+        callbacks = [EarlyStopping(patience=20)]
         exp.train(train_loader=train_loader,
                   valid_loader=valid_loader,
-                  epochs=100,
+                  epochs=250,
                   lr_schedulers=lr_schedulers,
                   callbacks=callbacks)
         exp.test(test_loader)
