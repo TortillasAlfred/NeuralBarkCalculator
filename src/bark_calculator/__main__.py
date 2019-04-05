e234e7db4745d0f7b160669f343e941fdbd31378
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-04-04 22:11:47 -0400

    Batch_size to 8

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -98,11 +98,11 @@ def old_main():
 
     train_sampler, valid_sampler = get_train_valid_samplers(dataset,
                                                             train_percent=0.8)
-    train_loader = DataLoader(augmented_dataset, batch_size=4,
+    train_loader = DataLoader(augmented_dataset, batch_size=8,
                               sampler=train_sampler)
-    valid_loader = DataLoader(dataset, batch_size=4,
+    valid_loader = DataLoader(dataset, batch_size=8,
                               sampler=valid_sampler)
-    pure_loader = DataLoader(pure_dataset, batch_size=4,
+    pure_loader = DataLoader(pure_dataset, batch_size=8,
                              sampler=valid_sampler)
     module = B2B()
     optim = torch.optim.Adam(module.parameters(), lr=1e-3, weight_decay=1e-5)
