ac66e7253c8286847b453e72565ac336c73d55fd
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-04-11 13:50:51 -0400

    1024 output

--- src/bark_calculator_1/treatment_method/methods.py
+++ src/bark_calculator_1/treatment_method/methods.py
@@ -339,11 +339,11 @@ class V2(TreatmentMethod):
         ws_image[~(ws_image == 1)] = 0
 
         ws_copy = np.repeat(ws_image, 16, axis=0).repeat(16, axis=1)
-        treated_images.append(ws_image[first_idx:-last_idx])
+        treated_images.append(np.repeat(ws_image, 4, axis=0).repeat(4, axis=1))
         big_image_2 = np.copy(big_image)
         big_image[ws_copy == 1] = [0, 0, 0]
         big_image_2[~(ws_copy == 1)] = [0, 0, 0]
-        treated_images.append(rescale(image, 1/16)[first_idx:-last_idx])
+        treated_images.append(rescale(image, 1/4))
         treated_images.append(big_image)
         treated_images.append(big_image_2)
 
