b7917fcdc7a44ddb1bfb2288ca106719b532135c
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-06-04 13:08:00 -0400

    weighted ce

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -328,7 +328,7 @@ def new_new_main():
                      module=module,
                      device=torch.device("cuda:0"),
                      optimizer=optim,
-                     loss_function=CrossEntropyLoss(),
+                     loss_function=CrossEntropyLoss(weight=pos_weights),
                      metrics=[IOU()])
 
     lr_schedulers = [ReduceLROnPlateau(patience=10)]
