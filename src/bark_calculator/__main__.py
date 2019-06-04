a8daa1561e03f68fc8b8ff0220da1121b0667a17
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-06-04 00:12:51 -0400

    32 batch

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -316,9 +316,9 @@ def new_new_main():
 
     train_split, valid_split, test_split = get_splits(train_dataset)
 
-    train_loader = DataLoader(Subset(train_dataset, train_split), batch_size=16, shuffle=True)
-    valid_loader = DataLoader(Subset(valid_dataset, valid_split), batch_size=16)
-    test_loader = DataLoader(Subset(test_dataset, test_split), batch_size=16)
+    train_loader = DataLoader(Subset(train_dataset, train_split), batch_size=32, shuffle=True)
+    valid_loader = DataLoader(Subset(valid_dataset, valid_split), batch_size=32)
+    test_loader = DataLoader(Subset(test_dataset, test_split), batch_size=32)
 
     module = deeplabv3_resnet101()
 
