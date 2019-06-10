eb1f80dd6d60ae612db170178584c052726cffa8
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-06-10 15:51:07 -0400

    in place swish

--- src/bark_calculator/resnet.py
+++ src/bark_calculator/resnet.py
@@ -44,7 +44,7 @@ class BasicBlock(nn.Module):
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
-        self.swish = lambda x: x * F.sigmoid(x)
+        self.swish = lambda x: x.mul_(F.sigmoid(x))
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
@@ -85,7 +85,7 @@ class Bottleneck(nn.Module):
         self.bn2 = norm_layer(width)
         self.conv3 = conv1x1(width, planes * self.expansion)
         self.bn3 = norm_layer(planes * self.expansion)
-        self.swish = lambda x: x * F.sigmoid(x)
+        self.swish = lambda x: x.mul_(F.sigmoid(x))
         self.downsample = downsample
         self.stride = stride
 
@@ -136,7 +136,7 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
-        self.swish = lambda x: x * F.sigmoid(x)
+        self.swish = lambda x: x.mul_(F.sigmoid(x))
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
