54ef409918e29dd2d71ee8ee4dba245e2e235c7c
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-06-11 17:54:26 -0400

    fcn_decay

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -75,7 +75,7 @@ def main():
 
     module = fcn_resnet50()
 
-    optim = torch.optim.Adam(module.parameters(), lr=1e-2, weight_decay=1e-2)
+    optim = torch.optim.Adam(module.parameters(), lr=1e-2, weight_decay=1e-1)
     exp = Experiment(directory="/mnt/storage/mgodbout/Ecorcage/fcn_decay/",
                      module=module,
                      device=torch.device("cuda:1"),
