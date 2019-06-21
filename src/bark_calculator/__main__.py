4401e069f57cfd0b00fdbc57ff1b90d568def08c
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-06-21 00:59:56 -0400

    regime

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -141,7 +141,7 @@ def main(args):
                      monitor_metric='val_IntersectionOverUnion',
                      monitor_mode='max')
 
-    lr_schedulers = [ExponentialLR(gamma=0.95)]
+    lr_schedulers = [ExponentialLR(gamma=0.96)]
     callbacks = []
 
     for i, (crop_size, batch_size) in enumerate(zip([448], [7])):
@@ -149,7 +149,7 @@ def main(args):
 
         exp.train(train_loader=train_loader,
                   valid_loader=valid_loader,
-                  epochs=(1 + i) * 100,
+                  epochs=(1 + i) * 150,
                   lr_schedulers=lr_schedulers,
                   callbacks=callbacks)
 
