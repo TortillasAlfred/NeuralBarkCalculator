25c6e77d25cb9b190f92c258b6143ca02b7f006d
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-06-25 23:44:03 -0400

    change output folder to correct one

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -29,7 +29,7 @@ def generate_output_folders(root_dir):
     wood_types = ["epinette_gelee", "epinette_non_gelee", "sapin"]
     levels = [('combined_images', ['train', 'valid', 'test']), ('outputs', ['train', 'valid', 'test'])]
 
-    results_dir = os.path.join(root_dir, 'Images', 'results', 'tagged_2_3')
+    results_dir = os.path.join(root_dir, 'Images', 'results', 'tagged_3_3')
 
     def mkdirs_if_not_there(dir):
         if not os.path.isdir(dir):
@@ -143,8 +143,8 @@ def main(args):
 
     module = fcn_resnet50()
 
-    optim = torch.optim.Adam(module.parameters(), lr=1e-3, weight_decay=2e-3)
-    exp = Experiment(directory=os.path.join(args.root_dir, 'tagged_2_3/'),
+    optim = torch.optim.Adam(module.parameters(), lr=1e-3, weight_decay=3e-3)
+    exp = Experiment(directory=os.path.join(args.root_dir, 'tagged_3_3/'),
                      module=module,
                      device=torch.device(args.device),
                      optimizer=optim,
@@ -197,7 +197,7 @@ def main(args):
 
             del pure_batch
 
-            # if os.path.isfile('/mnt/storage/mgodbout/Ecorcage/Images/results/fcn_decay/{}'.format(fname)):
+            # if os.path.isfile('/mnt/storage/mgodbout/Ecorcage/Images/results/tagged_3_3/{}'.format(fname)):
             #     continue
 
             outputs = module(batch[0].to(torch.device(args.device)))
@@ -258,7 +258,7 @@ def main(args):
             plt.suptitle(suptitle)
             plt.tight_layout()
             # plt.show()
-            plt.savefig(os.path.join(args.root_dir, 'Images/results/fcn_decay/combined_images/{}/{}/{}').format(
+            plt.savefig(os.path.join(args.root_dir, 'Images/results/tagged_3_3/combined_images/{}/{}/{}').format(
                 wood_type, split, fname),
                         format='png',
                         dpi=900)
@@ -272,11 +272,11 @@ def main(args):
             dual = Image.fromarray(dual_outputs, mode='L')
             dual.save(
                 os.path.join(args.root_dir,
-                             'Images/results/fcn_decay/outputs/{}/{}/{}').format(wood_type, split, fname))
+                             'Images/results/tagged_3_3/outputs/{}/{}/{}').format(wood_type, split, fname))
 
             results_csv.append(running_csv_stats)
 
-    csv_file = os.path.join(args.root_dir, 'Images', 'results', 'fcn_decay', 'final_stats.csv')
+    csv_file = os.path.join(args.root_dir, 'Images', 'results', 'tagged_3_3', 'final_stats.csv')
 
     with open(csv_file, 'w') as f:
         csv_writer = csv.writer(f, delimiter='\t')
