1b61517e2a3b3fd3aa4d1d05611a693e26b6ac78
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-06-28 09:53:31 -0400

    first draft NeuralBarkCalculator

--- src/bark_calculator/models.py
+++ src/bark_calculator/models.py
@@ -48,3 +48,29 @@ def fcn_resnet50():
     classifier = FCNHead(inplanes, 3)
 
     return SimpleSegmentationModel(backbone, classifier)
+
+
+class NeuralBarkCalculator(nn.Module):
+
+    DEFAULT_MEAN = [0.7399, 0.6139, 0.4401]
+    DEFAULT_STD = [0.1068, 0.1272, 0.1271]
+
+    def __init__(self, model_path, mean=DEFAULT_MEAN, std=DEFAULT_STD, target_size=1024):
+        super().__init__()
+        self.model = fcn_resnet50()
+        self.model.load_state_dict(torch.load(model_path))
+        self.mean = mean
+        self.std = std
+        self.target_size = 1024
+
+    def predict(self, root_path):
+        dataset = self._preprocess_images(root_path)
+
+        output_path = join(root_path, 'results')
+        self._predict_images(dataset, output_path)
+
+    def _preprocess_images(self, root_path):
+        pass
+
+    def _predict_images(self, dataset, output_path):
+        pass
