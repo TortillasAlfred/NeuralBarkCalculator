db4eac657d96712539a16228851d81f37e971dd7
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-06-28 13:38:47 -0400

    delete bark_calculator_1

--- src/bark_calculator_1/experiments/experiments.py
+++
@@ -1,8 +0,0 @@
-class GenericExperiment:
-    def __init__(self, image_loader, treatment_method, processor):
-        self.image_loader = image_loader
-        self.treatment_method = treatment_method
-        self.processor = processor
-
-    def run(self):
-        self.processor.process(self.image_loader, self.treatment_method)
--- src/bark_calculator_1/image_loader/builder.py
+++
@@ -1,9 +0,0 @@
-from image_loader.loaders import *
-
-
-def build_from_image_loader_arg(image_loader):
-    if image_loader == "good_examples":
-        return GoodExamplesLoader()
-
-    else:
-        return FolderLoader(image_loader)
