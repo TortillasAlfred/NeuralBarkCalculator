a4a27a8bb773410481f01cb0b4975c2899652aa9
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-08-12 18:54:44 -0400

    448 pix

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -199,7 +199,7 @@ def main(args):
     lr_schedulers = [ExponentialLR(gamma=0.95)]
     callbacks = []
 
-    for i, (crop_size, batch_size) in enumerate(zip([224], [16])):
+    for i, (crop_size, batch_size) in enumerate(zip([448], [7])):
         train_loader = get_loader_for_crop_batch(crop_size, batch_size, train_split, mean, std, train_weights,
                                                  args.root_dir)
 
