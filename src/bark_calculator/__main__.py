625e2c2a8d5c723eae4403e278c1e134c55dfff4
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-08-26 17:26:03 -0400

    fix singleton batch prob

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -228,7 +228,7 @@ def main(args):
                       mode='max')
     ]
 
-    for i, (crop_size, batch_size) in enumerate(zip([448], [1])):
+    for i, (crop_size, batch_size) in enumerate(zip([448], [2])):
         train_loader = get_loader_for_crop_batch(crop_size, batch_size,
                                                  train_split, mean, std,
                                                  train_weights, args.root_dir)
