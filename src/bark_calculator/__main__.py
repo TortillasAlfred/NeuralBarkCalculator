0960a23778c66400ac809d127731451a3f78b3ca
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-09-17 11:11:08 -0400

    bs 30

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -226,7 +226,7 @@ def main(args):
                       mode='max')
     ]
 
-    for i, (crop_size, batch_size) in enumerate(zip([256], [32])):
+    for i, (crop_size, batch_size) in enumerate(zip([256], [30])):
         train_loader = get_loader_for_crop_batch(crop_size, batch_size,
                                                  train_split, mean, std,
                                                  train_weights, args.root_dir)
