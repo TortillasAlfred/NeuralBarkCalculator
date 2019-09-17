76f539e59a0ca1795d1b6d6d89926b3fbaf07dfc
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-09-16 20:43:01 -0400

    Change bs

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -227,7 +227,7 @@ def main(args):
                       mode='max')
     ]
 
-    for i, (crop_size, batch_size) in enumerate(zip([512], [12])):
+    for i, (crop_size, batch_size) in enumerate(zip([512], [10])):
         train_loader = get_loader_for_crop_batch(crop_size, batch_size,
                                                  train_split, mean, std,
                                                  train_weights, args.root_dir)
