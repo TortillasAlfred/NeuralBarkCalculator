1ccf64d9266713376d48ac54b565916c9b9ea72d
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-09-20 10:16:17 -0400

    wd_3_do_9

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -32,7 +32,7 @@ def generate_output_folders(root_dir):
     levels = [('combined_images', ['train', 'valid', 'test']),
               ('outputs', ['train', 'valid', 'test'])]
 
-    results_dir = os.path.join(root_dir, 'Images', 'results', 'wd_5_4_do_8')
+    results_dir = os.path.join(root_dir, 'Images', 'results', 'wd_3_do_9')
 
     def mkdirs_if_not_there(dir):
         if not os.path.isdir(dir):
@@ -211,10 +211,10 @@ def main(args):
                               pin_memory=False)
 
     # module = deeplabv3_efficientnet(n=5)
-    module = fcn_resnet50(dropout=0.8)
+    module = fcn_resnet50(dropout=0.9)
 
-    optim = torch.optim.Adam(module.parameters(), lr=1e-3, weight_decay=5e-4)
-    exp = Experiment(directory=os.path.join(args.root_dir, 'wd_5_4_do_8'),
+    optim = torch.optim.Adam(module.parameters(), lr=1e-3, weight_decay=1e-3)
+    exp = Experiment(directory=os.path.join(args.root_dir, 'wd_3_do_9'),
                      module=module,
                      device=torch.device(args.device),
                      optimizer=optim,
@@ -372,7 +372,7 @@ def main(args):
             # plt.show()
             plt.savefig(os.path.join(
                 args.root_dir,
-                'Images/results/wd_5_4_do_8/combined_images/{}/{}/{}').format(
+                'Images/results/wd_3_do_9/combined_images/{}/{}/{}').format(
                     wood_type, split, fname),
                         format='png',
                         dpi=900)
@@ -388,12 +388,12 @@ def main(args):
             dual.save(
                 os.path.join(
                     args.root_dir,
-                    'Images/results/wd_5_4_do_8/outputs/{}/{}/{}').format(
+                    'Images/results/wd_3_do_9/outputs/{}/{}/{}').format(
                         wood_type, split, fname))
 
             results_csv.append(running_csv_stats)
 
-    csv_file = os.path.join(args.root_dir, 'Images', 'results', 'wd_5_4_do_8',
+    csv_file = os.path.join(args.root_dir, 'Images', 'results', 'wd_3_do_9',
                             'final_stats.csv')
 
     with open(csv_file, 'w') as f:
