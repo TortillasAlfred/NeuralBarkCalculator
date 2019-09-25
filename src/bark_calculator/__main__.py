981c0ce2bdfb49797d5af424059d7791f75a6c1d
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-09-25 00:02:13 -0400

    debug

--- src/bark_calculator/__main__.py
+++ src/bark_calculator/__main__.py
@@ -262,7 +262,7 @@ def main(args):
 
         exp.train(train_loader=train_loader,
                   valid_loader=valid_loader,
-                  epochs=(1 + i) * 300,
+                  epochs=(1 + i) * 30,
                   lr_schedulers=lr_schedulers,
                   callbacks=callbacks + [update_callback])
 
